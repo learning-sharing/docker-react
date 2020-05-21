0a6174025461535535d03678d81659d9d3922e6e
root <root@docker-centos.us-central1-a.c.academic-osprey-257611.internal>
2020-05-21 18:33:21 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Fuck Learn React
         </a>
       </header>
     </div>
